2308270ac85b1a8ef02c0e12987f16bbc666bc51
Ben Lee <leeohsunshine@gmail.com>
2016-03-04 21:24:01 -0500

    Dried some code in the main.js
    Fiddled with various other aspects of the files

--- js/main.js
+++ js/main.js
@@ -1,32 +1,65 @@
 
 // creating array of all prices, total of all prices,
 // average of all prices
-var prices = items.map(function (obj){
-		return obj.price
-});
-var total = prices.reduce(function (aPrice, bPrice){
-	return aPrice + bPrice
-}); 
 
-var avg = total / prices.length;
+// var prices = items.map(function (obj){
+// 		return obj.price
+// });
+// var total = prices.reduce(function (aPrice, bPrice){
+// 	return aPrice + bPrice
+// }); 
+
+// var avg = total / prices.length;
+// avg = avg.toFixed(2)
+
+function getAvg(arr){
+	if (arr.indexOf('Object' > -1)) {
+		return arr.map(function (obj){
+			return obj.price
+		}).reduce(function (a, b){
+			return a + b
+		}) / arr.length 
+	}
+}
 
 // Created Array of objects with prices between
 // 14 and 18 dollars
 
-var goodPrices = items.filter(function (obj){
-	if (obj.currency_code === "USD"){
-	 return obj.price > 14.00 && obj.price < 18.00}
-}).map(function (obj){
-	return obj.title
-});
+// var goodPrices = items.filter(function (obj){
+// 	if (obj.currency_code === "USD"){
+// 	 return obj.price > 14.00 && obj.price < 18.00}
+// }).map(function (obj){
+// 	return obj.title
+// });
+
+function getGoodPrices(arr){
+	if (arr.indexOf('Object' > -1)) {
+		return arr.filter(function (obj){
+			if (obj.currency_code === "USD"){
+			return obj.price > 14.00 && obj.price < 18.00}
+		}).map(function (obj){
+			return obj.title
+		})
+	}
+}
 
 // Retrieve item name with GBP currency code
 
-var gbpTitle = items.filter(function (obj){
-	if (obj['currency_code'] === "GBP") {
-		return obj
+// var gbpTitle = items.filter(function (obj){
+// 	if (obj['currency_code'] === "GBP") {
+// 		return obj
+// 	}
+// })[0].title
+
+function gbpTitle(arr){
+	if (arr.indexOf('Object' > -1)) {
+		return arr.filter(function (obj){
+			if (obj.currency_code === "GBP") {
+				return obj
+			}
+		})[0].title
 	}
-})[0].title
+}
 
 var gbpPrice = items.filter(function (obj){
 	if (obj['currency_code'] === "GBP") {
@@ -34,27 +67,76 @@ var gbpPrice = items.filter(function (obj){
 	}
 })[0].price
 
+// function gbpPrice(arr){
+// 	if (arr.indexOf('Object' > -1)) {
+// 		return arr.filter(function (obj){
+// 			if (obj.currency_code === "GBP") {
+// 				return obj
+// 			}
+// 		})[0].price
+// 	}
+// }
+
 // Display list of items made of wood
 
-var woodenItems = items.filter(function (obj){
-	if (obj.materials.indexOf('wood') > -1) {
-		return obj
+// var woodenItems = items.filter(function (obj){
+// 	if (obj.materials.indexOf('wood') > -1) {
+// 		return obj
+// 	}
+// }).map(function (obj){
+// 	return obj.title
+// });
+
+function listWoodenItems(arr){
+	if (arr.indexOf('Object' > -1)) {
+		return arr.filter(function (obj){
+			if (obj.materials.indexOf('wood') > -1) {
+				return obj
+			}
+		}).map(function (obj){
+			return obj.title
+		});
 	}
-}).map(function (obj){
-	return obj.title
-});
+}
 
 //Which ones made of more than 8 mats,
 // display title, number of items, 
 //and mats made of
 
-var mats8orMore = items.filter(function (obj){
+// var mats8orMore = items.filter(function (obj){
+// 	if (obj.materials.length >= 8) {
+// 		return obj
+// 	}
+// }).map(function (obj){
+// 	return obj.title + " has " + obj.materials.length + " materials"
+// })
+
+
+function listItems8OrMore(arr){
+	if (arr.indexOf('Object' > -1)) {
+		return arr.filter(function (obj){
+			if (obj.materials.length >= 8) {
+				return obj
+			}
+		}).map(function (obj){
+			return obj.title + " has " + obj.materials.length + " materials"
+		})
+	}
+}
+
+
+var listMats1 = items.filter(function (obj){
 	if (obj.materials.length >= 8) {
 		return obj
 	}
-}).map(function (obj){
-	return obj.title + " has " + obj.materials.length + " materials"
-})
+})[0].materials
+
+var listMats2 = items.filter(function (obj){
+	if (obj.materials.length >= 8) {
+		return obj
+	}
+})[1].materials
+
 //Items made by sellers total 
 
 var madeBySeller = items.filter(function (obj){
@@ -62,5 +144,23 @@ var madeBySeller = items.filter(function (obj){
 		return obj
 	}
 }).length
+ 
+madeBySeller = madeBySeller + " were made by their sellers" 
 
-madeBySeller = madeBySeller + " were made by their sellers"
+var one =  document.querySelector('.one');
+var two = document.querySelector('.two');
+var three = document.querySelector('.three');
+var four = document.querySelector('.four');
+var five = document.querySelector('.five');
+var six = document.querySelector('.six');
+one.innerHTML = getAvg(items).toFixed(2)
+two.innerHTML = getGoodPrices(items);
+three.innerHTML = gbpTitle(items) + " costs &pound;" + gbpPrice(items)
+four.innerHTML = listWoodenItems(items)
+five.innerHTML = "<div>" + listItems8OrMore(items) + "</div>" + "<div>" + listMats1 + "</div>"+ listMats2
+six.innerHTML = madeBySeller
+
+
+
+
+
